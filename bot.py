7530653745d3fce68318c16fa0fb4c8ac0f20956
marcochanyh <marcochanyh1015@gmail.com>
2019-12-29 13:40:57 +0800

    1. Added divide lines

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 import logging
-
+# ----- Tools ----- #
 def getButtonCallBackData(query):
     arg=query.data.split(',')
     return arg[0],int(arg[1]),int(arg[2]),int(arg[3]),arg[4]
@@ -17,14 +17,19 @@ def createButtonMarkup(textList,callbackKey,chatid):
 def sendButton(context,update,targetChatId,queryText,callBackKey,buttonList):
     reply_markup=createButtonMarkup(buttonList,callBackKey,str(update.effective_chat.id))
     context.bot.send_message(chat_id=targetChatId, text=queryText, reply_markup=reply_markup)
+    
+# ----- Game Logic ----- #
 
-
-
+# ----- Commands ----- #
 
 def start(update, context):
     sendButton(context,update,update.message.from_user.id,"Choose Game","ChooseGame",[["Dead Man's Draw","Option 2"],["Option 3"]])
 
 
+def help(update, context):
+    update.message.reply_text("Use /start to test this bot.")
+
+# ----- Call back handler ----- #
 def button_callBack(update, context):
     query = update.callback_query
     callBackKey,chatid,posY,posX,content=getButtonCallBackData(query)
@@ -32,11 +37,7 @@ def button_callBack(update, context):
     context.bot.send_message(chat_id=chatid, text='choosen:{},{},{}'.format(content,posY,posX))
 
 
-def help(update, context):
-    update.message.reply_text("Use /start to test this bot.")
-
-
-
+# ----- Startup calls ----- #
 updater = Updater('1043079672:AAGKrVHxXkROQTZG-T9CQ0gyjGw4hzEls5s', use_context=True)
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CallbackQueryHandler(button_callBack))
