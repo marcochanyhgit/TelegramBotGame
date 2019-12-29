8607df7945c602b87150ba5bfdfbb0239fb1252b
marcochanyh <marcochanyh1015@gmail.com>
2019-12-29 13:39:43 +0800

    1.added retrieve function in callback

--- bot.py
+++ bot.py
@@ -2,36 +2,34 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 import logging
 
-def hello(update, context):
-
-    update.message.reply_text(
-        "Hello {}, from :  , chat :  , message : .".format(update.message.from_user.first_name))
-    context.bot.send_message(chat_id=update.message.from_user.id, text="I'm a bot, please talk to me! {} ".format(update.message.from_user.id))
+def getButtonCallBackData(query):
+    arg=query.data.split(',')
+    return arg[0],int(arg[1]),int(arg[2]),int(arg[3]),arg[4]
 
 def createButtonMarkup(textList,callbackKey,chatid):
     keyboard=[]
     for i in range(len(textList)):
         keyboard.append([])
         for i2 in range(len(textList[i])):
-            keyboard[i].append(InlineKeyboardButton(textList[i][i2],callback_data="{},{},{},{}".format(callbackKey,chatid,i,i2)))
+            keyboard[i].append(InlineKeyboardButton(textList[i][i2],callback_data="{},{},{},{},{}".format(callbackKey,chatid,i,i2,textList[i][i2])))
     return InlineKeyboardMarkup(keyboard)
-
-
 
 def sendButton(context,update,targetChatId,queryText,callBackKey,buttonList):
     reply_markup=createButtonMarkup(buttonList,callBackKey,str(update.effective_chat.id))
     context.bot.send_message(chat_id=targetChatId, text=queryText, reply_markup=reply_markup)
 
+
+
+
 def start(update, context):
     sendButton(context,update,update.message.from_user.id,"Choose Game","ChooseGame",[["Dead Man's Draw","Option 2"],["Option 3"]])
 
 
-def button(update, context):
+def button_callBack(update, context):
     query = update.callback_query
-    
-    query.edit_message_text(text="Selected option: {}, chatid:{}".format(query.data,update.effective_chat.id))
-    arg=query.data.split(',')
-    context.bot.send_message(chat_id=int(arg[1]), text='choosen:{},{},{}'.format(arg[0],arg[2],arg[3]))
+    callBackKey,chatid,posY,posX,content=getButtonCallBackData(query)
+    query.edit_message_text(text="Selected : {}".format(content))
+    context.bot.send_message(chat_id=chatid, text='choosen:{},{},{}'.format(content,posY,posX))
 
 
 def help(update, context):
@@ -41,8 +39,7 @@ def help(update, context):
 
 updater = Updater('1043079672:AAGKrVHxXkROQTZG-T9CQ0gyjGw4hzEls5s', use_context=True)
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(button))
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CallbackQueryHandler(button_callBack))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 
 
