27e3d49008a2271fadb72246892c7f31a8b65129
marcochanyh <marcochanyh1015@gmail.com>
2019-12-30 00:08:28 +0800

    1. Added card

--- DeadManDrawGame.py
+++ DeadManDrawGame.py
@@ -14,16 +14,84 @@ class DeadManDrawGame():
         gameData[str(chatid)]["CurrentPlayer"]=0
         gameData[str(chatid)]["CardsPile"]=[]
         gameData[str(chatid)]["CardsOutside"]=[]
+        gameData[str(chatid)]["GravePile"]=[]
         context.bot.send_message(chat_id=chatid, text="(press /join to join game)")
         return
 
     # ----- Game Logic ----- #
     def GenerateCards(self):
         deck = []
-        deck.append(Card("A1"))
+
         deck.append(Card("A2"))
         deck.append(Card("A3"))
         deck.append(Card("A4"))
+        deck.append(Card("A5"))
+        deck.append(Card("A6"))
+        deck.append(Card("A7"))
+
+        deck.append(Card("B2"))
+        deck.append(Card("B3"))
+        deck.append(Card("B4"))
+        deck.append(Card("B5"))
+        deck.append(Card("B6"))
+        deck.append(Card("B7"))
+        
+        deck.append(Card("C2"))
+        deck.append(Card("C3"))
+        deck.append(Card("C4"))
+        deck.append(Card("C5"))
+        deck.append(Card("C6"))
+        deck.append(Card("C7"))
+        
+        deck.append(Card("D2"))
+        deck.append(Card("D3"))
+        deck.append(Card("D4"))
+        deck.append(Card("D5"))
+        deck.append(Card("D6"))
+        deck.append(Card("D7"))
+        
+        deck.append(Card("E2"))
+        deck.append(Card("E3"))
+        deck.append(Card("E4"))
+        deck.append(Card("E5"))
+        deck.append(Card("E6"))
+        deck.append(Card("E7"))
+        
+        deck.append(Card("F2"))
+        deck.append(Card("F3"))
+        deck.append(Card("F4"))
+        deck.append(Card("F5"))
+        deck.append(Card("F6"))
+        deck.append(Card("F7"))
+        
+        deck.append(Card("G2"))
+        deck.append(Card("G3"))
+        deck.append(Card("G4"))
+        deck.append(Card("G5"))
+        deck.append(Card("G6"))
+        deck.append(Card("G7"))
+        
+        deck.append(Card("H4"))
+        deck.append(Card("H5"))
+        deck.append(Card("H6"))
+        deck.append(Card("H7"))
+        deck.append(Card("H8"))
+        deck.append(Card("H9"))
+        
+        deck.append(Card("I2"))
+        deck.append(Card("I3"))
+        deck.append(Card("I4"))
+        deck.append(Card("I5"))
+        deck.append(Card("I6"))
+        deck.append(Card("I7"))
+        
+        deck.append(Card("J2"))
+        deck.append(Card("J3"))
+        deck.append(Card("J4"))
+        deck.append(Card("J5"))
+        deck.append(Card("J6"))
+        deck.append(Card("J7"))
+
         random.shuffle(deck)
         return deck
 
@@ -51,25 +119,26 @@ class DeadManDrawGame():
     def OpenCard(self, update, context, chatid, posY, posX, content, fromid):
         # find any thing to do after opened card #
         context.bot.send_message(chat_id=chatid,text="Opened Card"+gameData[str(chatid)]["CardsPile"][0].key)
-        # TODO: Add to card deck and check icon same or not #
-
-        # If explode
-        # self.CollectDeck(update, context, chatid, posY, posX, content, fromid, status)
-
         gotCard=gameData[str(chatid)]["CardsPile"].pop(0)
-        gameData[str(chatid)]["CardsOutside"].append(gotCard)
-        gotCard.OpenAction(update,context,chatid,posY,posX,content,gameData[str(chatid)]["JoinListName"][gameData[str(chatid)]["CurrentPlayer"]],self)
+        if(self.CheckCardOutsideExist(gameData[str(chatid)]["CardsOutside"],gotCard)):
+            context.bot.send_message(chat_id=chatid,text="Busted!")
+            self.CollectDeck(update, context, chatid, posY, posX, content, fromid, "Bust")
+            self.NextPlayer(update,context,chatid,posY,posX,content,fromid)
+        else:
+            gameData[str(chatid)]["CardsOutside"].append(gotCard)
+            gotCard.OpenAction(update,context,chatid,posY,posX,content,gameData[str(chatid)]["JoinListName"][gameData[str(chatid)]["CurrentPlayer"]],self)
         return
 
     def GiveUp(self,update,context,chatid,posY,posX,content,fromid):
         # TODO:Collect all card to deck #
-        self.CollectDeck(update, context, chatid, posY, posX, content, fromid, status)
-        
+        context.bot.send_message(chat_id=chatid,text="GiveUp!")
+        self.CollectDeck(update, context, chatid, posY, posX, content, fromid, "GiveUp")
         self.NextPlayer(update,context,chatid,posY,posX,content,fromid)
         return
 
     def CollectDeck(self,update,context,chatid,posY,posX,content,fromid,status):
         #collect starting deck to current player deck
+        #other card throw to grave yard
         return
 
     def NextPlayer(self,update,context,chatid,posY,posX,content,fromid):
@@ -123,3 +192,8 @@ class DeadManDrawGame():
         else:
             return False,"" 
 
+    def CheckCardOutsideExist(self,cardList,card):
+        for i in cardList:
+            if(i.skill==card.skill):
+                return True
+        return False
